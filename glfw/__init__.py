ca1b409641da61a3edb04dd42c9faef8580129fe
Brian Bruggeman <brian.m.bruggeman@gmail.com>
2016-01-04 17:35:58 -0600

    Bump version

--- glfw/__init__.py
+++ glfw/__init__.py
@@ -49,7 +49,7 @@ modname = os.path.basename(os.path.dirname(__file__))
 
 ###############################################################################
 __title__ = 'glfw-cffi'
-__version__ = '0.1.2'
+__version__ = '0.1.3-dev'
 __author__ = 'Brian Bruggeman'
 __email__ = 'brian.m.bruggeman@gmail.com'
 __license__ = 'Apache 2.0'
