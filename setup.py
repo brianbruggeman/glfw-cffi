64cc64953e86ca5918937c0bd96d16c16cba3d85
Brian Bruggeman <brian.m.bruggeman@gmail.com>
2016-01-10 19:32:46 -0600

    Cleanup

--- setup.py
+++ setup.py
@@ -74,41 +74,12 @@ class PyTest(TestCommand):
 
     def run_tests(self):
         import pytest
-        # from pkg_resources import normalize_path, _namespace_packages
-
-        # # Purge modules under test from sys.modules. The test loader will
-        # # re-import them from the build location. Required when 2to3 is used
-        # # with namespace packages.
-        # if sys.version_info >= (3,) and getattr(self.distribution, 'use_2to3', False):
-        #     # module = self.test_args[-1].split('.')[0]
-        #     module = self.test_package_name
-        #     if module in _namespace_packages:
-        #         del_modules = []
-        #         if module in sys.modules:
-        #             del_modules.append(module)
-        #         module += '.'
-        #         for name in sys.modules:
-        #             if name.startswith(module):
-        #                 del_modules.append(name)
-        #         map(sys.modules.__delitem__, del_modules)
-
-        #     # Run on the build directory for 2to3-built code
-        #     # This will prevent the old 2.x code from being found
-        #     # by py.test discovery mechanism, that apparently
-        #     # ignores sys.path..
-        #     ei_cmd = self.get_finalized_command("egg_info")
-
-        #     # Replace the module name with normalized path
-        #     # self.test_args[-1] = normalize_path(ei_cmd.egg_base)
-        #     self.test_args.append(normalize_path(ei_cmd.egg_base))
-
         errno = pytest.main(self.test_args)
         sys.exit(errno)
 
 tests_require = [
     'pytest',
     'pytest-flake8',
-    'pytest-pep8',
     'pytest-cov',
 ]
 
@@ -136,6 +107,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython'
         'Programming Language :: Python :: Implementation :: PyPy'
         ],
     install_requires=[
