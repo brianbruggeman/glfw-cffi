b8ff9c83faf620ce8e60374124f7a00904f75351
Brian Bruggeman <brian.m.bruggeman@gmail.com>
2016-01-12 10:00:34 -0600

    Updated logic and now controls colors

--- examples/shaders.py
+++ examples/shaders.py
@@ -4,6 +4,7 @@ from __future__ import division
 import ctypes
 from random import random as rand
 from textwrap import dedent as dd
+from pprint import pprint as pp
 
 import numpy as np
 import OpenGL
@@ -15,9 +16,11 @@ from glfw import gl
 # ######################################################################
 # Data
 # ######################################################################
+title = 'OpenGL 4.1 Rendering'
 width, height = 640, 480
 major, minor = (4, 1)
-draw_array = True
+draw_array = False
+use_data = True
 
 modes = sorted([
     gl.GL_POINTS,
@@ -34,39 +37,56 @@ modes = sorted([
     gl.GL_TRIANGLES_ADJACENCY,
     # gl.GL_PATCHES,
 ])
-mode_index = modes.index(gl.GL_TRIANGLE_STRIP)
+mode_index = modes.index(gl.GL_TRIANGLES)
 
 fills = [
     gl.GL_FILL,
     gl.GL_POINT,
     gl.GL_LINE
 ]
-fill_index = fills.index(gl.GL_POINT)
+fill_index = fills.index(gl.GL_LINE)
 
 pt = 0.5
 
-vertices = np.array((
-    [(x, y) for x in [-pt, 0, pt] for y in [-pt, 0, pt]]
-), dtype=np.float32)
+vertices = np.array([
+    (x, y) for x in [-pt, 0, pt] for y in [-pt, 0, pt]
+    # (0.0, pt),
+    # (-pt, -pt),
+    # (pt, -pt),
+    # (pt, pt),
+    # (-pt, pt),
+], dtype=np.float32)
 
 indices = np.array([
-    # _ for _ in range(vertices.shape[0])[:3]
-    0, 2, 4
+    # index for index in range(vertices.shape[0])
+    5, 6, 0,
+    5, 2, 0,
+    5, 8, 6,
 ], dtype=np.uint32)
 
 # Generate some colors for the points
 rgb = 3
 colors = np.array([
     # [rand() for _ in range(rgb)]  # vec3 of colors
-    (0.2, 1.0, 0.2)
-    for v in vertices  # one for every index
+    # for v in vertices  # one for every vertex
+    (1.0, 0.0, 0.0),
+    (0.0, 1.0, 0.0),
+    (0.0, 0.0, 1.0),
+
+    (1.0, 0.0, 1.0),
+    (0.0, 1.0, 1.0),
+    (1.0, 1.0, 0.0),
+
+    (0.5, 0.5, 0.5),
+    (0.5, 0.5, 0.0),
+    (0.0, 0.5, 0.5),
 ], dtype=np.float32)
 
 data = np.zeros(
     len(vertices),
     dtype=[
-        ("position", np.float32, vertices.shape[-1]),
-        ("color", np.float32, colors.shape[-1]),
+        ('position', np.float32, vertices.shape[-1]),
+        ('color', np.float32, colors.shape[-1]),
     ]
 )
 
@@ -74,33 +94,35 @@ data = np.zeros(
 data['position'] = vertices
 data['color'] = colors
 
-for index in indices:
-    print data[index]
+for i, d in enumerate(data):
+    print(i, d)
 
 
 vshader = '''
     #version 410
 
-    in vec3 position;
+    in vec2 position;
     in vec3 color;
 
-    out vec3 vcolor;
+    out vec3 v_color;
+
 
     void main () {
-        gl_Position = vec4(position, 1.0);
-        vcolor = color;
+        gl_Position = vec4(position, 0.0, 1.0);
+        v_color = color;
     }
     '''
 
 fshader = '''
     #version 410
 
-    in vec3 vcolor;
-    out vec4 frag_color;
+    in vec3 v_color;
+
+    out vec4 frag_colour;
 
     void main () {
-        frag_color = vec4(vcolor.xyz * 3.5, 1.0);
-        // frag_color = vec4(0.2, 1.0, 0.4, 1.0);
+        frag_colour = vec4(v_color, 1.0);
+        // frag_colour = vec4(0.5, 1.0, 0.5, 1.0);
     }
     '''
 
@@ -114,14 +136,14 @@ def on_key(win, key, code, action, mods):
     global fill_index
     global draw_array
     global indices_buffer_id
-    global vertices_buffer_id
     global vertices
     global colors
     global data
+    global use_data
     if action in [glfw.PRESS]:
         if key in [glfw.KEY_ESCAPE, glfw.KEY_Q]:
             # Quit
-            glfw.set_window_should_close(win, gl.GL_TRUE)
+            glfw.core.set_window_should_close(win, gl.GL_TRUE)
         elif key == glfw.KEY_M:
             # Update draw mode (points, lines, triangles, quads, etc.)
             if mods & glfw.MOD_SHIFT:
@@ -136,16 +158,8 @@ def on_key(win, key, code, action, mods):
             else:
                 fill_index = fill_index + 1 if fill_index + 1 < len(fills) else 0
             print('New fill: {}'.format(fills[fill_index]))
-        elif key == glfw.KEY_A:
-            # Toggle between drawing element with an indices and an array
-            draw_array = False if draw_array else True
-            gl.glEnableVertexAttribArray(0)
-            if not draw_array:
-                gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, indices_buffer_id)
-            gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vertices_buffer_id)
-            gl.glVertexAttribPointer(0, vertices.shape[-1], gl.GL_FLOAT, False, 0, None)
-            print('Drawing array: {}'.format(draw_array))
         elif key == glfw.KEY_SPACE:
+            # Randomize colors
             colors = np.array([
                 [rand() for _ in range(rgb)]  # vec3 of colors
                 for v in vertices  # one for every index
@@ -154,40 +168,43 @@ def on_key(win, key, code, action, mods):
             print(data)
 
 
+def compile(shader):
+    '''Compiles a shader'''
+
+
+def compile_and_link_program(vertex_shader, fragment_shader, geometry_shader=None,
+                             tesellation_evaluation=None, tesellation_control=None):
+    '''Creates a program from the shaders provided'''
+    shaders = {
+        'v': gl.create_shader(gl.VERTEX_SHADER),
+        'f': gl.create_shader(gl.FRAGMENT_SHADER),
+    }
+    program = gl.create_program()
+
+
+
 # ######################################################################
 # Setup OpenGL Context
-glfw.init()
-glfw.core.window_hint(glfw.SAMPLES, 4)  # 4x anti-aliasing
+glfw.core.init()
+glfw.core.window_hint(glfw.SAMPLES, 4)
 glfw.core.window_hint(glfw.CONTEXT_VERSION_MAJOR, major)
 glfw.core.window_hint(glfw.CONTEXT_VERSION_MINOR, minor)
 glfw.core.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-glfw.core.window_hint(glfw.OPENGL_FORWARD_COMPAT, gl.GL_TRUE)
-win = glfw.create_window(title='OpenGL 4.1 Rendering', width=width, height=height)
+glfw.core.window_hint(glfw.OPENGL_FORWARD_COMPAT, True)
+# glfw.core.window_hint(glfw.DECORATED, False)
+glfw.core.window_hint(glfw.RED_BITS, 8)
+glfw.core.window_hint(glfw.GREEN_BITS, 8)
+glfw.core.window_hint(glfw.BLUE_BITS, 8)
+glfw.core.window_hint(glfw.ALPHA_BITS, 8)
+glfw.core.window_hint(glfw.DEPTH_BITS, 24)
+
+win = glfw.create_window(title=title, width=width, height=height)
 glfw.core.set_key_callback(win, on_key)
 glfw.core.make_context_current(win)
 
 gl.glEnable(gl.GL_DEPTH_TEST)
 gl.glDepthFunc(gl.GL_LESS)
 
-# ######################################################################
-# Setup VBOs
-buffer_id = gl.glGenBuffers(1)
-gl.glBindBuffer(gl.GL_ARRAY_BUFFER, buffer_id)
-gl.glBufferData(gl.GL_ARRAY_BUFFER, data.nbytes, data, gl.GL_DYNAMIC_DRAW)
-
-vertices_buffer_id = gl.glGenBuffers(1)
-gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vertices_buffer_id)
-gl.glBufferData(gl.GL_ARRAY_BUFFER, vertices.nbytes, vertices.flatten(), gl.GL_STATIC_DRAW)
-
-colors_buffer_id = gl.glGenBuffers(1)
-gl.glBindBuffer(gl.GL_ARRAY_BUFFER, colors_buffer_id)
-gl.glBufferData(gl.GL_ARRAY_BUFFER, colors.nbytes, colors.flatten(), gl.GL_STATIC_DRAW)
-
-indices_buffer_id = gl.glGenBuffers(1)
-gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, indices_buffer_id)
-gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, indices, gl.GL_STATIC_DRAW)
-
-# ######################################################################
 # Build pipeline
 program = gl.glCreateProgram()
 vertex_shader = gl.glCreateShader(gl.GL_VERTEX_SHADER)
@@ -234,21 +251,8 @@ gl.glDetachShader(program, fragment_shader)
 gl.glDeleteShader(vertex_shader)
 gl.glDeleteShader(fragment_shader)
 
-# ######################################################################
-# Bind buffers
-vao = gl.glGenVertexArrays(1)
-gl.glBindVertexArray(vao)
-
-# gl.glEnableVertexAttribArray(0)
-# if draw_array:
-#     gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, indices_buffer_id)
-# gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vertices_buffer_id)
-# # gl.glBindBuffer(gl.GL_ARRAY_BUFFER, colors_buffer_id)
-# gl.glVertexAttribPointer(0, vertices.shape[-1], gl.GL_FLOAT, False, 0, None)
-
-# Set bindpoints for variables
-gl.glBindAttribLocation(program, 0, "position")
-gl.glBindAttribLocation(program, 1, "color")
+gl.glBindAttribLocation(program, 0, 'position')
+gl.glBindAttribLocation(program, 1, 'color')
 
 
 # ######################################################################
@@ -256,36 +260,45 @@ gl.glBindAttribLocation(program, 1, "color")
 gl.glClearColor(0.1, 0.1, 0.1, 1.0)
 
 # ######################################################################
+# Setup VBO and VAO
+vao = gl.glGenVertexArrays(1)
+buffer_id = gl.glGenBuffers(1)
+indices_buffer_id = gl.glGenBuffers(1)
+gl.glBindBuffer(gl.GL_ARRAY_BUFFER, buffer_id)
+
+gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, indices_buffer_id)
+gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, indices.flatten(), gl.GL_STATIC_DRAW)
+
+
+# ######################################################################
 # Render
-while not glfw.core.window_should_close(win):
+while not glfw.window_should_close(win):
     gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+    gl.glPolygonMode(gl.GL_FRONT_AND_BACK, fills[fill_index])
     gl.glEnable(gl.DEPTH_TEST)
     gl.glDepthFunc(gl.LESS)
-    gl.glPolygonMode(gl.GL_FRONT_AND_BACK, fills[fill_index])
     gl.glUseProgram(program)
+    gl.glBufferData(gl.GL_ARRAY_BUFFER, data.nbytes, data, gl.GL_DYNAMIC_DRAW)
 
-    # Use the VAO
     gl.glBindVertexArray(vao)
     stride = data.strides[0]
 
     offset = ctypes.c_void_p(0)
-    pos = gl.glGetAttribLocation(program, "position")
+    pos = gl.glGetAttribLocation(program, 'position')
     gl.glEnableVertexAttribArray(pos)
     gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, indices_buffer_id)
     gl.glBindBuffer(gl.GL_ARRAY_BUFFER, buffer_id)
     gl.glVertexAttribPointer(pos, data['position'].shape[-1], gl.GL_FLOAT, False, stride, offset)
 
-    offset = ctypes.c_void_p(data["position"].itemsize)
-    col = gl.glGetAttribLocation(program, "color")
+    offset = ctypes.c_void_p(data.dtype['position'].itemsize)
+    col = gl.glGetAttribLocation(program, 'color')
     gl.glEnableVertexAttribArray(col)
     gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, indices_buffer_id)
-    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vertices_buffer_id)
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, buffer_id)
     gl.glVertexAttribPointer(col, data['color'].shape[-1], gl.GL_FLOAT, False, stride, offset)
 
-    if draw_array:
-        gl.glDrawArrays(modes[mode_index], 0, len(vertices))
-    else:
-        gl.glDrawElements(modes[mode_index], len(indices), gl.GL_UNSIGNED_INT, None)
+    gl.glDrawElements(modes[mode_index], len(indices), gl.GL_UNSIGNED_INT, None)
+
     # Cleanup
     gl.glDisableVertexAttribArray(vao)
     # Standard Loop Event handling
