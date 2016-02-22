08e5903def81037613b89021357eee379a5d286a
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2016-02-21 23:19:39 -0600

    Fixes trove classifier

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython'
+        'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
         ],
     install_requires=install_requires,
